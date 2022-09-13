f2ad35eef43c6c17db437f97f36af857314be1c1
ALim <102565567+alimhanhan@users.noreply.github.com>
2022-09-13 16:14:11 +0900

    Add files via upload

---
+++ C++_09_13_1/example 2-1.cpp
@@ -0,0 +1,12 @@
+//cout, << 연산자를 사용하여 화면 출력해보기
+// 소스:SimpleC++.cpp
+
+#include <iostream>
+
+int main()  //C++프로그램은 main함수에서부터 실행 시작
+{
+	std::cout << "Hello\n";
+	std::cout << "첫 번째 맛보기 화면입니다.";
+
+	return 0;   //main 함수 종료 시 프로그램 종료
+}
