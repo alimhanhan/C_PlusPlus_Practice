29b67eb8b969f4aeacf8f7f1e674ac658903f58d
ALim <102565567+alimhanhan@users.noreply.github.com>
2022-09-15 00:47:00 +0900

    Add files via upload

---
+++ C++_09_13_7/example 2-7.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string song("Falling in love with you");
+	string elvis("Elvis Presley");
+	string singer;
+
+	cout << song + "을 부른 가수는";
+	cout << "(힌트: 첫 글자는 " << elvis[0] << ")?";
+
+	getline(cin, singer);
+
+	if (singer == elvis)
+	{
+		cout << "맞았습니다.";
+	}
+	else
+	{
+		cout << "틀렸습니다." + elvis + "입니다." << endl;
+	}
+}
