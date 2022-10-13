89e95a93f935fbf39ec17a7af8c5fec8d4880020
Leona <102565567+alimhanhan@users.noreply.github.com>
2022-10-13 11:02:50 +0900

    Add files via upload

---
+++ C++_10_04_5/예제 5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool equalArray(int* p, int* q, int size);
+
+int main()
+{
+	int a[] = { 1,2,3,4,5 };
+	int b[] = { 1,2,3,4,5 };
+
+	if (equalArray(a, b, 5))
+	{
+		cout << "arrays equal" << "\n";
+	}
+	else
+	{
+		cout << "arrays not equal" << "\n";
+	}
+}
+
+bool equalArray(int* p, int* q, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (*p != *q)
+		{
+			return false;
+		}
+		p++;
+		q++;
+	}
+	return true;
+}
