a5fd241c4fd6111a33a25a8cb7389782563e4baa
Leona <102565567+alimhanhan@users.noreply.github.com>
2022-12-24 15:08:31 +0900

    Add files via upload

---
+++ C++ 12_06_5/예제 4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// 2개의 제네릭 타입 T1, T2를 가지는 copy()의 템플릿
+template <class T1, class T2>
+void mcopy(T1 src[], T2 dest[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		dest[i] = (T2)src[i]; //T1 타입의 값을 T2 타입으로 변환한다.
+	}
+}
+
+int main()
+{
+	int x[] = { 1,2,3,4,5 };
+	double d[5];
+	char c[5] = { 'H','e','l','l','o' };
+	char e[5];
+
+	mcopy(x, d, 5);
+	mcopy(c, e, 5);
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << d[i] << ' ';
+	}
+	cout << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << e[i] << ' ';
+	}
+	cout << endl;
+}
